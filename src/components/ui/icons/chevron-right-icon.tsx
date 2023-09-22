7b1ce7a08031c917f8100604d8c7f77ccda83a1e
lordelogos <paulehiks@gmail.com>
2023-09-22 17:01:18 +0100

    wip: setup icons

---
+++ src/components/ui/icons/chevron-right-icon.tsx
@@ -0,0 +1,10 @@
+import { Icon, IconProps } from "@chakra-ui/react";
+
+export const ChevronRightIcon = (props: IconProps) => (
+  <Icon viewBox="0 0 16 16" fill="none" {...props}>
+    <path
+      d="M6.70739 12.7074L11.4141 8.00006L6.70739 3.29272L5.29272 4.70739L8.58606 8.00006L5.29272 11.2927L6.70739 12.7074Z"
+      fill="currentColor"
+    />
+  </Icon>
+);
---
+++ src/components/ui/icons/search-icon.tsx
@@ -0,0 +1,13 @@
+import { Icon, IconProps } from "@chakra-ui/react";
+
+export const SearchIcon = (props: IconProps) => (
+  <Icon viewBox="0 0 14 14" fill="none" {...props}>
+    <path
+      d="M12.625 12.625L9.90625 9.90625M11.375 6.375C11.375 9.13642 9.13642 11.375 6.375 11.375C3.61358 11.375 1.375 9.13642 1.375 6.375C1.375 3.61358 3.61358 1.375 6.375 1.375C9.13642 1.375 11.375 3.61358 11.375 6.375Z"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </Icon>
+);
