00e12587887ceda32e3836ea033eb96a84000df0
lordelogos <paulehiks@gmail.com>
2023-09-23 06:51:51 +0100

    wip: bounty details

---
+++ src/components/bounty-details/index.tsx
@@ -0,0 +1,66 @@
+import { Heading, Badge, Flex, Box, Text } from "@chakra-ui/react";
+import { VerifiedTickIcon } from "@ui/icons/verified-tick-icon";
+
+type BountyDetailsProps = {
+  title: string;
+  price: number;
+  dueBy: string;
+  skills: string[];
+};
+
+export const BountyDetails = ({
+  title,
+  price,
+  dueBy,
+  skills,
+}: BountyDetailsProps) => {
+  return (
+    <>
+      <Box
+        display={"flex"}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+      >
+        <Heading fontSize={"sm"} color={"#060606"}>
+          {title}
+        </Heading>
+        <Badge
+          bg={"rgba(6, 153, 85, 0.12)"}
+          color={"rgba(6, 153, 85, 1)"}
+          fontSize={"sm"}
+          p={"7px 5px"}
+          borderRadius={"5px"}
+        >
+          ${price.toFixed(2)}
+        </Badge>
+      </Box>
+      <Box display={"flex"} flexDirection={"column"} gap={"11px"}>
+        <Flex alignItems={"center"} justifyContent={"space-between"}>
+          <Text fontSize={"11px"}>
+            Payment Verified <VerifiedTickIcon />
+          </Text>
+          <Text fontSize={"10px"} color={"#F50449"}>
+            Due by:{" "}
+            <Text as="span" fontWeight={"bold"}>
+              {dueBy}
+            </Text>
+          </Text>
+        </Flex>
+        <Flex alignItems={"center"} gap={"7px"} flexWrap={"wrap"} pb={"4px"}>
+          {skills.map((skill, index) => (
+            <Badge
+              key={index}
+              p={"6px 7px"}
+              rounded={"full"}
+              fontSize={"9px"}
+              fontWeight={"normal"}
+              textTransform={"capitalize"}
+            >
+              {skill}
+            </Badge>
+          ))}
+        </Flex>
+      </Box>
+    </>
+  );
+};
--- src/pages/view-bounty.tsx
+++ src/pages/view-bounty.tsx
@@ -1,22 +1,12 @@
+import { BountyDetails } from "@/components/bounty-details";
 import { BaseLayout } from "@/components/layouts/base-layout";
 import { TaskBoardHeader } from "@/components/task-board-header";
 import { Breadcrumb, BreadcrumbItem } from "@/components/ui/breadcrumb";
 import { ChevronRightIcon } from "@/components/ui/icons/chevron-right-icon";
-import { VerifiedTickIcon } from "@/components/ui/icons/verified-tick-icon";
-import {
-  Badge,
-  Box,
-  Button,
-  Container,
-  Flex,
-  Grid,
-  GridItem,
-  Heading,
-  Text,
-} from "@chakra-ui/react";
+import { Button, Container, Flex, Grid, GridItem } from "@chakra-ui/react";
 
 export const ViewBounty = () => {
-  const { title, price, dueBy, skills } = {
+  const bounty = {
     title: "Sample bug fix for product listing API",
     price: 200,
     dueBy: "7 Oct, 2023",
@@ -56,56 +46,7 @@ export const ViewBounty = () => {
         </Flex>
         <Grid templateColumns={"2fr 1fr"} gap={"14px"}>
           <GridItem display={"flex"} flexDirection={"column"} gap={"15px"}>
-            <Box
-              display={"flex"}
-              alignItems={"center"}
-              justifyContent={"space-between"}
-            >
-              <Heading fontSize={"sm"} color={"#060606"}>
-                {title}
-              </Heading>
-              <Badge
-                bg={"rgba(6, 153, 85, 0.12)"}
-                color={"rgba(6, 153, 85, 1)"}
-                fontSize={"sm"}
-                p={"7px 5px"}
-                borderRadius={"5px"}
-              >
-                ${price.toFixed(2)}
-              </Badge>
-            </Box>
-            <Box display={"flex"} flexDirection={"column"} gap={"11px"}>
-              <Flex alignItems={"center"} justifyContent={"space-between"}>
-                <Text fontSize={"11px"}>
-                  Payment Verified <VerifiedTickIcon />
-                </Text>
-                <Text fontSize={"10px"} color={"#F50449"}>
-                  Due by:{" "}
-                  <Text as="span" fontWeight={"bold"}>
-                    {dueBy}
-                  </Text>
-                </Text>
-              </Flex>
-              <Flex
-                alignItems={"center"}
-                gap={"7px"}
-                flexWrap={"wrap"}
-                pb={"4px"}
-              >
-                {skills.map((skill, index) => (
-                  <Badge
-                    key={index}
-                    p={"6px 7px"}
-                    rounded={"full"}
-                    fontSize={"9px"}
-                    fontWeight={"normal"}
-                    textTransform={"capitalize"}
-                  >
-                    {skill}
-                  </Badge>
-                ))}
-              </Flex>
-            </Box>
+            <BountyDetails {...bounty} />
           </GridItem>
         </Grid>
       </Container>
